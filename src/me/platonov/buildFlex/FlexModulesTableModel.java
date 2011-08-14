f9c5bc7e12ab6142520dcd8b7d8be7f0cf24d9b6
Chaim Platonov <chaim.platonov@gmail.com>
2011-08-14 11:17:03 +0300

    # show only flex facets # use table instead of tree

---
+++ src/me/platonov/buildFlex/FlexModulesTableModel.java
@@ -0,0 +1,111 @@
+package me.platonov.buildFlex;
+
+import com.intellij.lang.javascript.flex.FlexFacet;
+import com.intellij.lang.javascript.flex.FlexFacetConfigurationImpl;
+
+import javax.swing.*;
+import javax.swing.event.EventListenerList;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author chaim
+ * @since 8/11/11 3:30 PM
+ */
+public class FlexModulesTableModel implements TableModel {
+
+    protected List<FlexFacet> list = new ArrayList<FlexFacet>();
+    protected EventListenerList listenerList = new EventListenerList();
+
+    public static final String[] columns = {/*icon*/"", "Name", "Build"};
+    private static final Class<?>[] columnTypes = {/*icon*/Icon.class, String.class, Boolean.class};
+    public static final Integer[] columnMaxWidth = {25, null, 25};
+
+    public void add(FlexFacet item) {
+        list.add(item);
+        fireTableChanged(new TableModelEvent(this, list.indexOf(item)));
+    }
+
+    public void clear() {
+        list.clear();
+        fireTableChanged(new TableModelEvent(this));
+    }
+
+    @Override
+    public int getRowCount() {
+        return list.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columns.length;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return columns[columnIndex];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return columnTypes[columnIndex];
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return columnTypes[columnIndex] == Boolean.class;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        final FlexFacet flexFacet = list.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return flexFacet.getType().getIcon();
+            case 1:
+                return flexFacet.getModule().getName();
+            case 2:
+                final FlexFacetConfigurationImpl configuration = (FlexFacetConfigurationImpl) flexFacet.getConfiguration();
+                return configuration.getFlexBuildConfiguration().DO_BUILD;
+        }
+
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        if (2 == columnIndex) {
+            final FlexFacet flexFacet = list.get(rowIndex);
+            final FlexFacetConfigurationImpl configuration = (FlexFacetConfigurationImpl) flexFacet.getConfiguration();
+            configuration.getFlexBuildConfiguration().DO_BUILD = ((Boolean) aValue);
+        } else {
+            throw new IllegalArgumentException("Can't update this column");
+        }
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+        listenerList.add(TableModelListener.class, l);
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+        listenerList.remove(TableModelListener.class, l);
+    }
+
+    public void fireTableChanged(TableModelEvent e) {
+        // Guaranteed to return a non-null array
+        Object[] listeners = listenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TableModelListener.class) {
+                ((TableModelListener) listeners[i + 1]).tableChanged(e);
+            }
+        }
+    }
+
+}
---
+++ src/me/platonov/buildFlex/FlexModulesToolWindow.java
@@ -0,0 +1,83 @@
+package me.platonov.buildFlex;
+
+import com.intellij.facet.Facet;
+import com.intellij.facet.impl.DefaultFacetsProvider;
+import com.intellij.lang.javascript.flex.FlexFacet;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+
+import javax.swing.*;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author chaim
+ * @since 8/8/11 - 4:49 PM
+ */
+public class FlexModulesToolWindow implements ToolWindowFactory {
+
+    private JPanel thePanel;
+    private JButton refreshProjectTreeButton;
+    private JTable theTable;
+
+    private Project myProject;
+
+    FlexModulesTableModel model = new FlexModulesTableModel();
+
+    public FlexModulesToolWindow() {
+        refreshProjectTreeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateFacets();
+            }
+        });
+    }
+
+    @Override
+    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+        myProject = project;
+
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(thePanel, "", false);
+        toolWindow.getContentManager().addContent(content);
+
+
+        theTable.setAutoCreateColumnsFromModel(true);
+        theTable.setAutoscrolls(true);
+
+        theTable.setTableHeader(new JTableHeader());
+        theTable.setModel(model);
+
+        for (int i = 0; i < theTable.getColumnModel().getColumnCount(); i++) {
+            TableColumn column = theTable.getColumnModel().getColumn(i);
+            if (null != FlexModulesTableModel.columnMaxWidth[i]) {
+                column.setMaxWidth(FlexModulesTableModel.columnMaxWidth[i]);
+            }
+        }
+
+        updateFacets();
+    }
+
+    private void updateFacets() {
+        model.clear();
+
+        Module[] modules = ModuleManager.getInstance(myProject).getModules();
+        for (Module module : modules) {
+            final Facet[] allFacets = DefaultFacetsProvider.INSTANCE.getAllFacets(module);
+            for (Facet facet : allFacets) {
+                if (facet instanceof FlexFacet) {
+                    model.add((FlexFacet) facet);
+                }
+            }
+        }
+    }
+
+
+}
